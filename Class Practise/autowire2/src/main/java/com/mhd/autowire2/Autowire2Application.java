d1ea85e06e478437ca2c9107beb5e648eaceb918
mhdrkb <mhdrkb91@gmail.com>
2019-02-06 09:15:20 +0600

    autowired

---
+++ Class Practise/autowire2/src/main/java/com/mhd/autowire2/Autowire2Application.java
@@ -0,0 +1,14 @@
+package com.mhd.autowire2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autowire2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Autowire2Application.class, args);
+    }
+
+}
+
