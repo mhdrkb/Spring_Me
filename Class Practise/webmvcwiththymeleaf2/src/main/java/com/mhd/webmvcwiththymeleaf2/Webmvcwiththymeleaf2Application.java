3213ea5b2ed8a564e1bb72a8af640b2610d09a82
mhdrkb <mhdrkb91@gmail.com>
2019-02-09 09:21:37 +0600

    save to database

---
+++ Class Practise/webmvcwiththymeleaf2/src/main/java/com/mhd/webmvcwiththymeleaf2/Webmvcwiththymeleaf2Application.java
@@ -0,0 +1,14 @@
+package com.mhd.webmvcwiththymeleaf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webmvcwiththymeleaf2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webmvcwiththymeleaf2Application.class, args);
+    }
+
+}
+
---
+++ Class Practise/webmvcwiththymeleaf2/src/main/java/com/mhd/webmvcwiththymeleaf2/entity/User.java
@@ -0,0 +1,78 @@
+package com.mhd.webmvcwiththymeleaf2.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2 , max = 30)
+    private String name;
+
+    @Min(value = 18, message = "Hey , minimum age is 18")
+    private byte age;
+
+    public User() {
+    }
+
+    public User(@NotNull @Size(min = 2, max = 30) String name, @Min(value = 18, message = "Hey , minimum age is 18") byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
