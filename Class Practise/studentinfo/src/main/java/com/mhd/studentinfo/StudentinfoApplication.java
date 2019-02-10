46158e9465498708b35e2b22ff6b7155042d770e
mhdrkb <mhdrkb91@gmail.com>
2019-02-10 09:43:44 +0600

    studentinfo

---
+++ Class Practise/studentinfo/src/main/java/com/mhd/studentinfo/StudentinfoApplication.java
@@ -0,0 +1,14 @@
+package com.mhd.studentinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentinfoApplication.class, args);
+    }
+
+}
+
