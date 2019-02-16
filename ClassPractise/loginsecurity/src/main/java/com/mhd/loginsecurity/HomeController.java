d969c819d968c57fdf2ff693ebf3f277381d86c9
mhdrkb <mhdrkb91@gmail.com>
2019-02-16 12:58:43 +0600

    update

--- ClassPractise/loginsecurity/src/main/java/com/mhd/loginsecurity/HomeController.java
+++ ClassPractise/loginsecurity/src/main/java/com/mhd/loginsecurity/HomeController.java
@@ -1,11 +1,29 @@
 package com.mhd.loginsecurity;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/login"})
     public String index(){
-        return "index";
+        return "login";
+    }
+    @GetMapping("/user/u")
+    public String user(){
+        return "user/user";
+    }
+    @GetMapping("/admin/h")
+    public String home(){
+        return "admin/home";
+    }
+    @GetMapping("/public/r")
+    public String regi(){
+        return "public/regi";
+    }
+    @GetMapping("/secure/s")
+    public String secure(){
+        return "secure/service";
     }
 }
