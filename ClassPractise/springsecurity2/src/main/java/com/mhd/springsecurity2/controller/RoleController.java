cb80d8345de8430933ac73b3686e081e2bfdb640
mhdrkb <mhdrkb91@gmail.com>
2019-02-20 11:59:57 +0600

    templatting

--- ClassPractise/springsecurity2/src/main/java/com/mhd/springsecurity2/controller/RoleController.java
+++ ClassPractise/springsecurity2/src/main/java/com/mhd/springsecurity2/controller/RoleController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,24 +17,27 @@ public class RoleController {
     private RoleRepo roleRepo;
 
     @GetMapping(value = "/role-list")
-    public String roleList(Model model){
-        model.addAttribute("roleList",this.roleRepo.findAll());
+    public String roleList(Model model) {
+        model.addAttribute("roleList", this.roleRepo.findAll());
         return "role/role-list";
     }
+
+
     @GetMapping("/add-role")
     public String role(Role role) {
         return "role/add-role";
     }
+
     @PostMapping("/add-role")
     public String saveRole(@Valid Role role, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "role/add-role";
         } else {
-            if(role != null){
+            if (role != null) {
                 Role role1 = this.roleRepo.findByRoleName(role.getRoleName());
-                if (role1 != null){
-                    model.addAttribute("existMsg","RoleName ALready Exist");
-                }else{
+                if (role1 != null) {
+                    model.addAttribute("existMsg", "RoleName ALready Exist");
+                } else {
                     role.setRoleName(role.getRoleName().toUpperCase());
                     this.roleRepo.save(role);
                     model.addAttribute("role", new Role()); //to clear the form after successfully reloading
@@ -45,6 +46,40 @@ public class RoleController {
             }
         }
         return "role/add-role";
+    }
+
+
+    @GetMapping("/edit-role/{id}")
+    public String roleEdit(@PathVariable("id") Long id, Model model, Role role) {
+        model.addAttribute("role", this.roleRepo.getOne(id));
+        return "role/edit-role";
+    }
+
+    @PostMapping("/edit-role/{id}")
+    public String editRole(@Valid Role role, @PathVariable("id") Long id, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "role/edit-role";
+        } else {
+            if (role != null) {
+                Role role1 = this.roleRepo.findByRoleName(role.getRoleName());
+                if (role1 != null) {
+                    model.addAttribute("existMsg", "RoleName ALready Exist");
+                    return "role/edit-role";
+                } else {
+                    role.setRoleName(role.getRoleName().toUpperCase());
+                    this.roleRepo.save(role);
+                    model.addAttribute("role", new Role()); //to clear the form after successfully reloading
+                    model.addAttribute("successMsg", "Congratulations you have successfully saved Role data....");
+                }
+            }
+        }
+        return "redirect: /role-list";
+    }
+
+    @GetMapping(value = "/del-role/{id}")
+    public String delRole(@PathVariable("id") Long id) {
+        this.roleRepo.deleteById(id);
+        return "redirect: /role-list";
     }
 //    @GetMapping(value = "/role-save") //HIT JUST ONE TIME AND SAVE THE ROLES INTO DATABASE
 //    public String saveRole() {
---
+++ ClassPractise/templatting/src/main/java/com/mhd/templatting/TemplattingApplication.java
@@ -0,0 +1,13 @@
+package com.mhd.templatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplattingApplication.class, args);
+    }
+
+}
